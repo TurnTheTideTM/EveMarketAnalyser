393b3f449e853cf932016902ef0e571a3fd08c27
TurnTheTideTM <richard.viehoever@yahoo.de>
2014-01-28 19:41:56 +0100

    ID statt name

--- main/Item.py
+++ main/Item.py
@@ -3,5 +3,5 @@ __author__ = 'RV Administrator'
 
 class Item(object):
     def __init__(self):
-        self.name = ""
+        self.id = -1
         self.buyorders = []  # Eine Liste, die Dictionaries im Stil von {ort: preis} enthält. Nur die aktuellen.
