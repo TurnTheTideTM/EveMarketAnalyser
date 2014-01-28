bbd7dbabbdf1765b377d972fc96261c63d64f630
TurnTheTideTM <richard.viehoever@yahoo.de>
2014-01-28 19:36:37 +0100

    Aktuellere Itemdatenbank hinzugefügt

--- main/Order.py
+++ main/Order.py
@@ -1,3 +1,7 @@
 __author__ = 'RV Administrator'
 
-class Item(object): pass
+
+class Item(object):
+    def __init__(self):
+        self.name = ""
+        self.buyorders = []  # Eine Liste, die Dictionaries im Stil von {ort: preis} enthält.
