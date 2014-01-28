bd3d9be9bb0c439de8a84ba0d22ca20f084f1ba3
WorldSEnder <worldsbegin@gmx.de>
2014-01-28 11:07:25 -0800

    Reorganisiertes buy und sell-Order Buch

--- main/Item.py
+++ main/Item.py
@@ -1,7 +1,34 @@
 __author__ = 'RV Administrator'
 
+from .OrderBook import OrderBook, Mode
 
-class Item(object):
+#class Item(object):
+#    def __init__(self):
+#        self.id = -1
+#        self.buyorders = []  # Eine Liste, die Dictionaries im Stil von {ort: preis} enthält. Nur die aktuellen.
+
+class TradingBook:
     def __init__(self):
-        self.id = -1
-        self.buyorders = []  # Eine Liste, die Dictionaries im Stil von {ort: preis} enthält. Nur die aktuellen.
+        self._idToOrders = {}
+        
+    def setItem(self, itemId, orderBookB, orderBookS):
+        if orderBookB is None or orderBookB.getMode() == Mode.SELL:
+            orderBookB = OrderBook(Mode.BUY) # neues buy order book generieren
+            
+        if orderBookS is None or orderBookS.getMode() == Mode.BUY:
+            orderBookS = OrderBook(Mode.SELL) # neues buy order book generieren
+            
+        self._idToOrders.update({itemId: [orderBookB, orderBookS]})
+        
+    '''
+    Returns the assciated order and buy book.
+    '''
+    def getItem(self, itemId):
+        if itemId not in self._idToOrders.keys():
+            orderB = OrderBook(Mode.BUY)
+            orderS = OrderBook(Mode.SELL)
+            self._idToOrders.update({itemId: [orderB, orderS]})
+        return self._idToOrders.get(itemId)[:] ## shallowcopy of what is in the list
+        
+    def getBuyOrderBook(self, itemId):
+        return self._idToOrders.get(itemId, {}).get('b', OrderBook())
---
+++ main/OrderBook.py
@@ -0,0 +1,18 @@
+'''
+Created on 28.01.2014
+
+@author: Carbon
+'''
+
+class Mode:
+    BUY = 0
+    SELL = 1
+
+class OrderBook:
+    def __init__(self, mode):
+        self._mode = mode # Für backreference und zur
+                            # Vermeidung von versehentlichen puts
+        self.orders = []
+        
+    def getMode(self):
+        return self._mode
