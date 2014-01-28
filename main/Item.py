f58c37b9122a3166db930cbf66109b9804327181
WorldSEnder <worldsbegin@gmx.de>
2014-01-28 11:13:32 -0800

    Da war ne standard lib für das :)

--- main/Item.py
+++ main/Item.py
@@ -1,6 +1,7 @@
 __author__ = 'RV Administrator'
 
 from .OrderBook import OrderBook, Mode
+from collections import defaultdict
 
 #class Item(object):
 #    def __init__(self):
@@ -9,25 +10,15 @@ from .OrderBook import OrderBook, Mode
 
 class TradingBook:
     def __init__(self):
-        self._idToOrders = {}
+        self._idToOrders = defaultdict(lambda: [OrderBook(Mode.BUY), OrderBook(Mode.SELL)])
         
-    def setItem(self, itemId, orderBookB, orderBookS):
-        if orderBookB is None or orderBookB.getMode() == Mode.SELL:
-            orderBookB = OrderBook(Mode.BUY) # neues buy order book generieren
-            
-        if orderBookS is None or orderBookS.getMode() == Mode.BUY:
-            orderBookS = OrderBook(Mode.SELL) # neues buy order book generieren
-            
+    def setItem(self, itemId, orderBookB, orderBookS):            
         self._idToOrders.update({itemId: [orderBookB, orderBookS]})
         
     '''
     Returns the assciated order and buy book.
     '''
     def getItem(self, itemId):
-        if itemId not in self._idToOrders.keys():
-            orderB = OrderBook(Mode.BUY)
-            orderS = OrderBook(Mode.SELL)
-            self._idToOrders.update({itemId: [orderB, orderS]})
         return self._idToOrders.get(itemId)[:] ## shallowcopy of what is in the list
         
     def getBuyOrderBook(self, itemId):
