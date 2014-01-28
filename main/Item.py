d2562e7f48462640e28e6cdfc1ba33967388c962
TurnTheTideTM <richard.viehoever@yahoo.de>
2014-01-28 19:41:14 +0100

    Renamed

---
+++ main/Item.py
@@ -0,0 +1,7 @@
+__author__ = 'RV Administrator'
+
+
+class Item(object):
+    def __init__(self):
+        self.name = ""
+        self.buyorders = []  # Eine Liste, die Dictionaries im Stil von {ort: preis} enthält. Nur die aktuellen.
--- main/Order.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'RV Administrator'
-
-
-class Item(object):
-    def __init__(self):
-        self.name = ""
-        self.buyorders = []  # Eine Liste, die Dictionaries im Stil von {ort: preis} enthält.
